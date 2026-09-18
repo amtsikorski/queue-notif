056e27091ac1596bc4b24d22aadffa14711611ff
ali <amtsikorski@gmail.com>
2026-09-17 21:11:46 -0500

    v 1.0 published
    basic styling and mvp. includes queue monitoring, alerts, popup, and
    help page. add images for help page and toolbar.

--- content.js
+++ content.js
@@ -4,13 +4,10 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendReponse) {
     if (params.action === 'start') {
         console.log('starting monitor')
         chrome.storage.local.set({
-            webhook: params.webhook,
-            threshold: params.threshold,
-            frequency: params.frequency,
             tabId: params.tabId,
             monitoringActive: true
         });
-        startQueueMonitor(params.webhook, params.frequency, params.threshold);
+        startQueueMonitor(params.webhook, params.frequency, params.userId, params.threshold);
         chrome.runtime.sendMessage({ action: "started" });
     } else if (params.action === 'stop') {
         console.log('clearing interval: ' + notifInterval);
@@ -19,39 +16,53 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendReponse) {
 });
 
 let notifInterval = null;
-function startQueueMonitor(webhook, frequency, threshold) {
-    function getPosition() {
-        const el = document.querySelector(".StatusBox_mainText__9gJXJ strong");
-        if (!el) return null;
-        const pos = el.innerText;
-        return pos;
+let checkCount = 0;
+let checksPerReport = 0;
+function getPosition() {
+    const el = document.querySelector(".StatusBox_mainText__9gJXJ strong");
+    if (!el) {
+        console.warn("Queue position element not found.");
+        return 0;
     }
-    function postPosition(webhook, threshold) {
-        const pos = getPosition();
-        if (pos !== null) {
-            console.log(`Queue position: ${pos}`);
-            let posInt = pos.replace(',', "");
-            posInt = parseInt(posInt);
-            if (posInt <= threshold) {
-                console.log("send warning");
-                // fetch(webhook, {
-                //   method: "POST",
-                //   headers: { "Content-Type": "application/json" },
-                //   body: JSON.stringify({ content: `!!! WARNING: Your queue position is now ${pos}!!!` })
-                // });
-            } else {
-                console.log("send update")
-                // fetch(webhook, {
-                //   method: "POST",
-                //   headers: { "Content-Type": "application/json" },
-                //   body: JSON.stringify({ content: `Update: Your queue position is now ${pos}!` })
-                // });
-            }
-        } else {
-            console.warn("Queue position element not found.");
-        }
+    const pos = el.innerText;
+    console.log(`Queue position: ${pos}`);
+    return pos;
+}
+
+function postPosition(webhook, content) {
+    fetch(webhook, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ content: content })
+    });
+}
+
+function checkPosition(webhook, userId, threshold) {
+    const pos = getPosition();
+    let posInt = pos.replace(',', "");
+    posInt = parseInt(posInt);
+    checkCount++;
+    console.log('pos: ' + posInt + ', checksCount: ' + checkCount)
+    if (posInt <= threshold) {
+        console.log("send mention update");
+        let warningMessage = `Warning <@${userId}>: Your queue position is now ${pos} !!!`;
+        postPosition(webhook, warningMessage);
+        return;
     }
-    const delay = 60000 * frequency;
-    postPosition(webhook, threshold);
-    notifInterval = setInterval(postPosition, delay, webhook, threshold);
+
+    if (checkCount >= checksPerReport) {
+        console.log("send update")
+        let updateMessage = `Update: Your queue position is now ${pos}!`;
+        postPosition(webhook, updateMessage);
+        checkCount = 0;
+    }
+}
+
+function startQueueMonitor(webhook, frequency, userId, threshold) {
+    const checkInterval = 60000; //every minute
+    checksPerReport = (frequency * 60 * 1000) / checkInterval;
+    const initPos = getPosition();
+    const initialUpdate = `Start: Your queue position is ${initPos}!`
+    postPosition(webhook, initialUpdate);
+    notifInterval = setInterval(checkPosition, checkInterval, webhook, userId, threshold);
 }
